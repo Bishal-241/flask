72732c66daec8f887598dab7d080b34ced96ce5b
Bishal Shah <anjalishahjhau12345@gmail.com>
2023-06-02 12:47:41 +0545

    Templates structuring

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask , render_template
+
+# INITIALIZING FLASK APPLICATION
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template()
+    return render_template("index.jinja")
 
 @app.route('/products')
 def product():
     return render_template("product.jinja")
 
 
+
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 8000)
